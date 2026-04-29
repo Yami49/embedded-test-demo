51ad2e2b397d4975c4ad07b9b4728c0077a35af0
Georgios Gerontidis <84520906+Yami49@users.noreply.github.com>
2026-04-29 11:21:38 +0200

    Update test_temperature_controller.cpp

--- tests/test_temperature_controller.cpp
+++ tests/test_temperature_controller.cpp
@@ -1,1 +1,14 @@
+#include <gtest/gtest.h>
+#include "../src/temperature_controller.h"
 
+TEST(TemperatureControllerTest, HeaterTurnsOnBelowTarget) {
+    TemperatureController controller(25.0);
+    controller.update(20.0);
+    EXPECT_TRUE(controller.isHeaterOn());
+}
+
+TEST(TemperatureControllerTest, HeaterTurnsOffAboveTarget) {
+    TemperatureController controller(25.0);
+    controller.update(30.0);
+    EXPECT_FALSE(controller.isHeaterOn());
+}
