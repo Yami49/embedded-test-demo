5cd390e830a8fc85ce58528b5d76d8f7fb766d90
Georgios Gerontidis <84520906+Yami49@users.noreply.github.com>
2026-04-29 11:21:23 +0200

    Update temperature_controller.cpp

--- src/temperature_controller.cpp
+++ src/temperature_controller.cpp
@@ -1,1 +1,16 @@
+#include "temperature_controller.h"
 
+TemperatureController::TemperatureController(double target)
+    : targetTemp(target), heaterOn(false) {}
+
+void TemperatureController::update(double currentTemp) {
+    if (currentTemp < targetTemp) {
+        heaterOn = true;
+    } else {
+        heaterOn = false;
+    }
+}
+
+bool TemperatureController::isHeaterOn() const {
+    return heaterOn;
+}
