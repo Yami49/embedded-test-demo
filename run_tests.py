3c13c94fff151f9502d6f2359d53ff12593b1588
Georgios Gerontidis <84520906+Yami49@users.noreply.github.com>
2026-04-29 11:22:08 +0200

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,1 +1,10 @@
+import os
 
+def run():
+    os.system("mkdir -p build")
+    os.system("cd build && cmake ..")
+    os.system("cd build && make")
+    os.system("cd build && ./runTests")
+
+if __name__ == "__main__":
+    run()
