a8eb95aa43ae860fece423cf7303b2f47e2866b6
Georgios Gerontidis <84520906+Yami49@users.noreply.github.com>
2026-04-29 11:21:01 +0200

    Update temperature_controller.h

--- src/temperature_controller.h
+++ src/temperature_controller.h
@@ -1,1 +1,17 @@
+#ifndef TEMPERATURE_CONTROLLER_H
+#define TEMPERATURE_CONTROLLER_H
 
+class TemperatureController {
+public:
+    TemperatureController(double target);
+
+    void update(double currentTemp);
+
+    bool isHeaterOn() const;
+
+private:
+    double targetTemp;
+    bool heaterOn;
+};
+
+#endif
